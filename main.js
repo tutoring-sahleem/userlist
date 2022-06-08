7d57045efa66fba64f842d9b4b52f0a9deb66162
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-06-08 12:51:21 -0700

    Co-authored-by: Leem85 <Leem85@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 // Create an application that takes an ARRAY of user OBJECTS and render them in a unordered list <ul></ul>
 
- const users = []
+const users = [
+  {
+    id: 0,
+    name: "kev",
+    age: 28,
+    canDrink: true,
+  },
+  {
+    id: 1,
+    name: "sal",
+    age: 18,
+    canDrink: true,
+  },
+  {
+    id: 2,
+    name: " bill",
+    age: 12,
+    canDrink: false,
+  },
+];
 
- const isAdult = function() {
+//  console.log('first user', users[0])
+const isAdult = function (user) {
+  if (user.age >= 18) {
+    return user;
+  } else {
+    return 'not an adult';
+  }
+};
 
- }
+for (let i = 0; i < users.length; i++) {
+    
+  const userBoolean = isAdult(users[i]);
+
+  console.log("our users age", users[i].age, userBoolean);
+}
 
 
-console.log('loaded main.js =====>')
-document.body.append('This is loading from main.js')
+
+console.log("loaded main.js =====>");
+document.body.append("This is loading from main.js");
