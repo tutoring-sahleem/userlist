8ab8f3f1c6509172132378aec2e3ca5b5d6d08b9
Kevin J Mosley <Kevinjmosley93@gmail.com>
2022-06-08 11:43:19 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// Create an application that takes an ARRAY of user OBJECTS and render them in a unordered list <ul></ul>
+
+ const users = []
+
+ const isAdult = function() {
+
+ }
+
+
+console.log('loaded main.js =====>')
+document.body.append('This is loading from main.js')
